eab000570703e41457421fb71b409a2c92d8a448
Admin <admin@Admins-MacBook-Pro.local>
2023-08-23 13:50:24 -0700

    the one with initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+
+import { Card, CardContent, Grid, Typography } from "@material-ui/core";
+
+
+const Home = () => {
+    return (
+    <Grid container>
+        <Grid item xs={12} style={{ padding: 20 }}>
+            <Typography variant="h4" component="h4">
+                Kendra Connector UI
+            </Typography>
+        </Grid>
+    </Grid>
+    )
+
+}
+
+export default Home;

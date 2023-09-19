ade408c5e507a8e2c82e84069d6b45ab4bff24d3
parulrajPTG <parul.raj@peopletech.com>
2023-09-19 11:27:25 -0700

    Added unit test case for KenraLandingPage. It will check for rendering of the following: 1. Title- AWS Kendra 2. Description under the title 3. Image

---
+++ src/tests/components_tests/KendraLandingPageTest.test.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {render, screen} from "@testing-library/react";
+import KendraLandingPage from "components/KendraLandingPage";
+
+describe('KendraLandingPage', () => {
+    it('renders the components of landing page without errors', () => {
+        const { getByText, getByAltText } = render(<KendraLandingPage />);
+
+        // Check if the component renders the title
+        const titleElement = getByText('AWS KENDRA');
+        expect(titleElement).toBeInTheDocument();
+
+        // Check if the component renders the description
+        const descriptionElement = getByText(/AWS Kendra is a highly accurate/i);
+        expect(descriptionElement).toBeInTheDocument();
+
+        // Check if the component renders the image
+        const imageElement = getByAltText('Image');
+        expect(imageElement).toBeInTheDocument();
+    });
+});
